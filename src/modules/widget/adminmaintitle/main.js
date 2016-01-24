19708adf2eb05d0c112bc7feeb4087952e5d669d
helinjiang <me@helinjiang.com>
2016-01-24 18:21:27 +0800

    修改循环

--- src/modules/widget/adminmaintitle/main.js
+++ src/modules/widget/adminmaintitle/main.js
@@ -3,16 +3,15 @@ var Vue = require('lib/vue');
 var MyComponent = Vue.extend({
     template: __inline('main.html'),
     filters: {
-        ifLast: function(v) {
-            console.log('--=', v, this.data);
-return true;
+        ifNotLast: function(v) {
+            return v < this.items.length - 1;
         }
     },
     data: function() {
         return {
             name: 'Blank Page',
             desc: 'This is blank page desc!',
-            data: [{
+            items: [{
                 name: 'Home',
                 url: 'index.html',
                 icon: 'home'
