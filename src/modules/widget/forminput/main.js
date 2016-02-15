6f08dd8c118bb33b6746653ec3f2182bd5e47d30
HeLinJiang <me@helinjiang.com>
2016-02-15 12:33:47 +0800

    forminput

--- src/modules/widget/forminput/main.js
+++ src/modules/widget/forminput/main.js
@@ -37,6 +37,59 @@ Vue.component('form-input', {
             type: String,
             required: true
         },
+
+        'autocomplete': {
+            type: String,
+            'default': 'on'
+        },
+
+        'horizontal': {
+            type: Boolean,
+            'default': false
+        },
+
+        /**
+         * 如果是水平排列的话，则需要定义左右的宽度，格式为x-x，其中x值为1到12
+         */
+        'col': {
+            type: String,
+            'default': '3-9'
+        }
+
+    },
+    computed: {
+        colLeft: function() {
+            var defaultVal = 3,
+                val;
+
+            if (!this.col) {
+                return defaultVal;
+            }
+
+            val = parseInt(this.col.split('-')[0], 10);
+
+            if (isNaN(val) || val < 1 || val > 12) {
+                return defaultVal;
+            } else {
+                return val;
+            }
+        },
+        colRight: function() {
+            var defaultVal = 9,
+                val;
+
+            if (!this.col) {
+                return defaultVal;
+            }
+
+            val = parseInt(this.col.split('-')[1], 10);
+
+            if (isNaN(val) || val < 1 || val > 12) {
+                return defaultVal;
+            } else {
+                return val;
+            }
+        }
     },
     ready: function() {
 
