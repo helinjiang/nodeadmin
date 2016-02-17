0c3b5c82a7ce482857d8b606c960dbb082fac021
helinjiang <me@helinjiang.com>
2016-02-17 11:21:37 +0800

    增加think-debug-toolbar工具

--- src/common/bootstrap/middleware.js
+++ src/common/bootstrap/middleware.js
@@ -11,3 +11,17 @@
  * })
  * 
  */
+
+'use strict';
+
+import debugToolbar from 'think-debug-toolbar';
+
+let conf = {
+    panels: ['request', 'session', 'view', 'template', 'response', 'config', 'info'],
+    depth: 4,
+    extra_attrs: '',
+    disabled: false,
+    sort: false
+};
+
+think.middleware('debug_toolbar', debugToolbar(conf));
