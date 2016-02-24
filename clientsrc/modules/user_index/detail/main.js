4df628fe5e8d8cb8dd03505e2f0701c46ab1baea
helinjiang <me@helinjiang.com>
2016-02-24 15:45:13 +0800

    user detail

--- clientsrc/modules/user_index/detail/main.js
+++ clientsrc/modules/user_index/detail/main.js
@@ -1,50 +1,39 @@
-var Vue = require('lib/vue');
+var CommonCrud = require('common/crud');
 
-var validator = require('common/validator');
-var Msg = require('components/msg/main');
-
-module.exports = Vue.extend({
+module.exports = CommonCrud.extend({
     template: __inline('main.html'),
-    data: function() {
-        return {
-            items: []
-        };
+    data: {
+        items: []
     },
     methods: {
-        showModal: function(data) {
-            this.items = [{
-                key: 'id',
-                value: data.id,
-                title: 'ID'
-            }, {
-                key: 'name',
-                value: data.name,
-                title: '用户名'
-            }, {
-                key: 'birthday',
-                value: data.birthday,
-                title: '生日'
-            }, {
-                key: 'stateShow',
-                value: data.stateShow,
-                title: '状态'
-            }, {
-                key: 'createTime',
-                value: data.createTime,
-                title: '创建时间'
-            }, {
-                key: 'updateTime',
-                value: data.updateTime,
-                title: '最后修改时间'
-            }];
-
-            this.$children[0].show();
-        },
-        hideModal: function() {
-            this.$children[0].hide();
+        beforeShowModal: function(data) {
+            if (data) {
+                this.items = [{
+                    key: 'id',
+                    value: data.id,
+                    title: 'ID'
+                }, {
+                    key: 'name',
+                    value: data.name,
+                    title: '用户名'
+                }, {
+                    key: 'birthday',
+                    value: data.birthday,
+                    title: '生日'
+                }, {
+                    key: 'stateShow',
+                    value: data.stateShow,
+                    title: '状态'
+                }, {
+                    key: 'createTime',
+                    value: data.createTime,
+                    title: '创建时间'
+                }, {
+                    key: 'updateTime',
+                    value: data.updateTime,
+                    title: '最后修改时间'
+                }];
+            }
         }
-    },
-    ready: function() {
-
     }
 });
--- www/static/all.js
+++ www/static/all.js
@@ -15455,53 +15455,44 @@ define('modules/user_index/detail/main', function(require, exports, module) {
 
   'use strict';
   
-  var Vue = require('modules/lib/vue');
+  var CommonCrud = require('modules/common/crud');
   
-  var validator = require('modules/common/validator');
-  var Msg = require('modules/components/msg/main');
-  
-  module.exports = Vue.extend({
+  module.exports = CommonCrud.extend({
       template: "<div class=\"deletepage\">\r\n    <modal title=\"用户信息详情\" v-on:confirm=\"hideModal\">\r\n        <table class=\"table table-bordered\">\r\n            <tr v-for=\"item in items\">\r\n                <th>{{ item.title}}</th>\r\n                <td>{{ item.value}}</td>\r\n            </tr>\r\n        </table>\r\n    </modal>\r\n</div>\r\n",
-      data: function data() {
-          return {
-              items: []
-          };
+      data: {
+          items: []
       },
       methods: {
-          showModal: function showModal(data) {
-              this.items = [{
-                  key: 'id',
-                  value: data.id,
-                  title: 'ID'
-              }, {
-                  key: 'name',
-                  value: data.name,
-                  title: '用户名'
-              }, {
-                  key: 'birthday',
-                  value: data.birthday,
-                  title: '生日'
-              }, {
-                  key: 'stateShow',
-                  value: data.stateShow,
-                  title: '状态'
-              }, {
-                  key: 'createTime',
-                  value: data.createTime,
-                  title: '创建时间'
-              }, {
-                  key: 'updateTime',
-                  value: data.updateTime,
-                  title: '最后修改时间'
-              }];
-  
-              this.$children[0].show();
-          },
-          hideModal: function hideModal() {
-              this.$children[0].hide();
+          beforeShowModal: function beforeShowModal(data) {
+              if (data) {
+                  this.items = [{
+                      key: 'id',
+                      value: data.id,
+                      title: 'ID'
+                  }, {
+                      key: 'name',
+                      value: data.name,
+                      title: '用户名'
+                  }, {
+                      key: 'birthday',
+                      value: data.birthday,
+                      title: '生日'
+                  }, {
+                      key: 'stateShow',
+                      value: data.stateShow,
+                      title: '状态'
+                  }, {
+                      key: 'createTime',
+                      value: data.createTime,
+                      title: '创建时间'
+                  }, {
+                      key: 'updateTime',
+                      value: data.updateTime,
+                      title: '最后修改时间'
+                  }];
+              }
           }
-      },
-      ready: function ready() {}
+      }
   });
 
 });
