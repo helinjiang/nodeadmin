8b3090512c1e57281aa3a9b6d7a896fe24758403
helinjiang <me@helinjiang.com>
2016-02-26 05:03:06 +0800

    尝试Model.js

--- www/static/modules/user_index/model.js
+++ www/static/modules/user_index/model.js
@@ -1,6 +1,5 @@
 define('modules/user_index/model', function(require, exports, module) {
 
-  
   /**
    * 描述定义
    * @type {Object}
@@ -15,6 +14,7 @@ define('modules/user_index/model', function(require, exports, module) {
       require: true, // 是否是必须的
       type: 'number', // 类型
       length: 11,
+      minLength: 1,
   
       isShowInIndex: true, // 是否在首页的表格中展示
       isShowInDetail: true, // 是否在详情页面展示
@@ -46,6 +46,7 @@ define('modules/user_index/model', function(require, exports, module) {
       require: true,
       type: 'string',
       length: 64,
+      minLength: 3,
   
       isShowInIndex: true,
       isShowInDetail: true,
@@ -56,13 +57,127 @@ define('modules/user_index/model', function(require, exports, module) {
       howToAdd: 'inputtext',
       howToModify: 'readonly',
   
-      indexDatagridOptions: {
-          // index: 0, // 在列表中的位置，越小则越靠前
-          // title:'ID', // 如果不设置则默认获取通用的title
-          // css:'idCss',
-          // disableOrder:false
-          // render:'xxx'       
-      }
+      indexDatagridOptions: {}
+  }, {
+      name: 'pwd',
+      title: '密码',
+      isDbTableField: true,
+      isPrimaryKey: false,
+      require: true,
+      type: 'string',
+      length: 64,
+      minLength: 3,
+  
+      isShowInIndex: false,
+      isShowInDetail: false,
+      isShowInDelete: false,
+      isShowInAdd: true,
+      isShowInModify: false,
+  
+      howToAdd: 'inputpassword',
+      howToModify: '',
+  
+      indexDatagridOptions: {}
+  }, {
+      name: 'birthday',
+      title: '生日',
+      isDbTableField: true,
+      isPrimaryKey: false,
+      require: true,
+      type: 'string', // 针对日期，此处也是不合理的
+      length: 10,
+      minLength: 1,
+  
+      isShowInIndex: true,
+      isShowInDetail: true,
+      isShowInDelete: true,
+      isShowInAdd: true,
+      isShowInModify: true,
+  
+      howToAdd: 'date',
+      howToModify: 'date',
+  
+      indexDatagridOptions: {}
+  }, {
+      name: 'state',
+      title: '状态',
+      isDbTableField: true,
+      isPrimaryKey: false,
+      require: true,
+      type: 'number',
+      length: 1,
+      minLength: 1,
+  
+      isShowInIndex: false,
+      isShowInDetail: false,
+      isShowInDelete: false,
+      isShowInAdd: true,
+      isShowInModify: true,
+  
+      howToAdd: 'select2',
+      howToModify: 'select2',
+  
+      indexDatagridOptions: {}
+  }, {
+      name: 'createTime',
+      title: '创建时间',
+      isDbTableField: true,
+      isPrimaryKey: false,
+      require: true,
+      type: 'string', // 针对日期，此处也是不合理的
+      length: 19,
+      minLength: 1,
+  
+      isShowInIndex: true,
+      isShowInDetail: true,
+      isShowInDelete: true,
+      isShowInAdd: false,
+      isShowInModify: false,
+  
+      howToAdd: 'autotime',
+      howToModify: '',
+  
+      indexDatagridOptions: {}
+  }, {
+      name: 'updateTime',
+      title: '更新时间',
+      isDbTableField: true,
+      isPrimaryKey: false,
+      require: true,
+      type: 'string', // 针对日期，此处也是不合理的
+      length: 19,
+      minLength: 1,
+  
+      isShowInIndex: true,
+      isShowInDetail: true,
+      isShowInDelete: true,
+      isShowInAdd: false,
+      isShowInModify: false,
+  
+      howToAdd: 'autotime',
+      howToModify: 'autotime',
+  
+      indexDatagridOptions: {}
+  }, {
+      name: 'stateShow',
+      title: '状态',
+      isDbTableField: true,
+      isPrimaryKey: false,
+      require: true,
+      type: 'number',
+      length: 1,
+      minLength: 1,
+  
+      isShowInIndex: false,
+      isShowInDetail: false,
+      isShowInDelete: false,
+      isShowInAdd: true,
+      isShowInModify: true,
+  
+      howToAdd: 'select2',
+      howToModify: 'select2',
+  
+      indexDatagridOptions: {}
   }];
   
   var common = {
