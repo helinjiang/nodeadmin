abe85537eee3c0466224c81db2d8a7a8408eeb87
helinjiang <me@helinjiang.com>
2016-02-29 06:16:41 +0800

    样式

--- www/static/modules/test_index/main.js
+++ www/static/modules/test_index/main.js
@@ -9,7 +9,7 @@ define('modules/test_index/main', function(require, exports, module) {
   var testWizard = require('modules/test_index/testwizard/main');
   
   module.exports = Vue.extend({
-      template: "<div class=\"test_index-main\">\r\n\r\n    <div class=\"row\">\r\n\r\n        <!-- <div class=\"col-md-6\">\r\n            <test-select2></test-select2>\r\n        </div>   \r\n        \r\n        <div class=\"col-md-6\">\r\n             <test-date></test-date>  \r\n        </div>   -->\r\n\r\n        <test-wizard></test-wizard>\r\n\r\n    </div>  \r\n    \r\n</div>",
+      template: "<div class=\"test_index-main\">\r\n\r\n    <div class=\"row\">\r\n\r\n        <!-- <div class=\"col-md-6\">\r\n            <test-select2></test-select2>\r\n        </div>   \r\n        \r\n        <div class=\"col-md-6\">\r\n             <test-date></test-date>  \r\n        </div>   -->\r\n\r\n        <div class=\"col-md-12\">\r\n            <test-wizard></test-wizard>\r\n        </div>\r\n\r\n    </div>  \r\n    \r\n</div>",
       components: {
           TestSelect2: TestSelect2,
           TestDate: TestDate,
