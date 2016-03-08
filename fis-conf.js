4e0e87821ca0b50f77d994b27e05c97aa1fe8130
helinjiang <me@helinjiang.com>
2016-03-08 15:17:10 +0800

    fisconfig

--- fis-conf.js
+++ fis-conf.js
@@ -120,29 +120,34 @@ fis.match("/pages/(*)/*(.html)", {
 
 
 fis.match('::packager', {
-    // npm install [-g] fis3-postpackager-loader
-    // 分析 __RESOURCE_MAP__ 结构，来解决资源加载问题
-    postpackager: fis.plugin('loader', {
-        resourceType: 'mod',
-        useInlineMap: true // 资源映射表内嵌
-    }),
-    packager: fis.plugin('map'),
-    spriter: fis.plugin('csssprites', {
-        layout: 'matrix',
-        margin: '15'
-    })
+        // npm install [-g] fis3-postpackager-loader
+        // 分析 __RESOURCE_MAP__ 结构，来解决资源加载问题
+        postpackager: fis.plugin('loader', {
+            resourceType: 'mod',
+            useInlineMap: true // 资源映射表内嵌
+        }),
+        packager: fis.plugin('map'),
+        spriter: fis.plugin('csssprites', {
+            layout: 'matrix',
+            margin: '15'
+        })
 
-}).match('/{common,components,modules,pages}/**.{css,scss}', {
-    packTo: '/static/all.css' //css打成一个包
-}).match('/common/{css,plugins}/**.{css,scss}', { //TODO此处待改正
-    packTo: ''
-}).match('{common,components,modules,pages}/**.js', {
-    packTo: '/static/all.js'
-}).match('/common/plugins/**.js', {
-    packTo: ''
-}).match('/common/lib/mod.js', {
-    packTo: '/static/mod.js'
-});
+    })
+    .match('/{common,components,modules,pages}/**.{css,scss}', {
+        packTo: '/static/all.css' //css打成一个包
+    })
+    .match('/common/{css,plugins}/**.{css,scss}', { //TODO此处待改正
+        packTo: ''
+    })
+    .match('{common,components,modules,pages}/**.js', {
+        packTo: '/static/all.js'
+    })
+    .match('/common/plugins/**.js', {
+        packTo: ''
+    })
+    .match('/common/lib/mod.js', {
+        packTo: '/static/mod.js'
+    });
 
 
 //生产环境下CSS、JS压缩合并
